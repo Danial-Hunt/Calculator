ebf91a83a38c126f412a558f62160fd0aa2be9ac
Danial-Hunt <76631486+Danial-Hunt@users.noreply.github.com>
2021-07-08 13:13:34 +0000

    Create main.py
    Code. No comments yet.

---
+++ main.py
@@ -0,0 +1,39 @@
+class Calculator:
+
+    def __init__(self):
+        pass
+
+    def multiply(self, x, y):
+        return x * y
+    
+    def divide(self, x, y):
+        return x / y
+    
+    def add(self, x, y):
+        return int(x) + int(y)
+    
+    def subtract(self, x, y):
+        return x - y
+
+
+def main():
+    c = Calculator()
+
+    problem = input("Problem: " )
+
+    addition_operator_location = problem.find('+')
+    subtraction_operator_location = problem.find('-')
+    division_operator_location = problem.find('/')
+    multiplication_operator_location = problem.find('*')
+    
+
+    first_number = problem[0:addition_operator_location]
+    second_number = problem[addition_operator_location:]
+    print(addition_operator_location)
+    second_number_cleaner = second_number.translate({ord('+'): None})
+    second_number_cleanest = second_number_cleaner.translate({ord(' '): None})
+    print(first_number)
+    print(second_number_cleanest)
+    print(c.add(first_number, second_number_cleanest))
+
+main()
